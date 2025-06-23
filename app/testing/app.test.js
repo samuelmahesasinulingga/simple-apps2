3a4d2ca0701247ee8aea900a11796542b5276611
samuelmahesasinulingga <sinulingga848@gmail.com>
2025-06-23 21:24:09 +0700

    update testing

--- app/testing/app.test.js
+++ app/testing/app.test.js
@@ -1,9 +1,6 @@
 const request = require('supertest');
-const mysql = require('mysql');
-require('dotenv').config();
-
-const connection = require('../middleware/db_connect');
-const app = require('../app'); // Replace with the path to your application file
+const app = require('../app'); // Path ke aplikasi express kamu
+const connection = require('../middleware/db_connect'); // Pastikan ini export connection/pool
 
 describe('Unit Test /', () => {
   it('should respond with index.html', async () => {
@@ -13,28 +10,23 @@ describe('Unit Test /', () => {
 });
 
 describe('Unit Test /app1', () => {
-    it('should respond with "Hello App1!"', async () => {
-      const response = await request(app).get('/app1');
-      expect(response.status).toBe(200);
-    });
+  it('should respond with "Hello App1!"', async () => {
+    const response = await request(app).get('/app1');
+    expect(response.status).toBe(200);
   });
+});
 
-  describe('Unit Test /app2', () => {
-    it('should respond with "Hello App2!"', async () => {
-      const response = await request(app).get('/app2');
-      expect(response.status).toBe(200);
-    });
+describe('Unit Test /app2', () => {
+  it('should respond with "Hello App2!"', async () => {
+    const response = await request(app).get('/app2');
+    expect(response.status).toBe(200);
   });
+});
 
 describe('Integration Test Connect Database', () => {
-  beforeEach(() => {
-    // Set the connection for the application to use
-    app.set('connection', connection.connect);
-  });
-
-  afterEach(() => {
-    // Close the connection pool after the tests are done
-    connection.end();
+  afterAll((done) => {
+    // Tutup koneksi pool atau connection setelah SEMUA test selesai
+    connection.end(done); // callback agar Jest tahu sudah selesai
   });
 
   it('should respond with users data', async () => {
